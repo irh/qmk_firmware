48fe753c5face2be9371c823d4ec600f677a9d56
irh <ian.r.hobson@gmail.com>
2019-08-04 22:05:40 +0200

    Iris tweaks

--- keyboards/keebio/iris/keymaps/irh/keymap.c
+++ keyboards/keebio/iris/keymaps/irh/keymap.c
@@ -8,11 +8,13 @@ extern keymap_config_t keymap_config;
 #define L_1 1
 #define L_2 2
 #define L_3 3
+#define L_4 4
 
 #define L1_BSPC LT(L_1, KC_BSPC)
 #define L1_SPACE LT(L_1, KC_SPACE)
 #define L2_TAB LT(L_2, KC_TAB)
 #define L2_ENTER LT(L_2, KC_ENTER)
+#define L3_F7 LT(L_3, KC_F7)
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -26,15 +28,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Layer 0 - Base QWERTY layer
   [L_0] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_F3,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MPLY,
+     LCTL(KC_UP),KC_Q, KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MPLY,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     LCAG(KC_H),TG(L_3),KC_LCTL,KC_LALT, KC_LCMD, L1_BSPC, L2_TAB,           L2_ENTER,L1_SPACE,KC_RCMD, KC_RALT, KC_RCTL, LCAG(KC_LEFT), LCAG(KC_RIGHT),
+     LCAG(KC_H),TG(L_4),KC_LCTL,KC_LALT, KC_LCMD, L1_BSPC, L2_TAB,           L2_ENTER,L1_SPACE,KC_RCMD, KC_RALT, KC_RCTL, LCAG(KC_LEFT), LCAG(KC_RIGHT),
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_F15,  KC_F16,  _______,                   _______, KC_F17,  KC_F18
+                                    KC_F5,   KC_F6,   L3_F7,                     KC_F8,   KC_F9,   KC_F10
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -69,9 +71,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                  `----+----+----'        `----+----+----'
   ),
 
-
-  // Layer 3 - Reverse of Colemak Mod DH software mapping to QWERTY
+  // Layer 3 - Arrow keys on the home row
   [L_3] = LAYOUT_kc(
+  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
+         ,    ,    ,    ,    ,    ,                   ,    ,UP  ,    ,    ,    ,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,                   ,LEFT,DOWN,RGHT,    ,    ,
+  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,                   ,    ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,    ,         ,    ,    ,    ,    ,    ,    ,
+  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
+                           ,    ,    ,             ,    ,
+  //                  `----+----+----'        `----+----+----'
+  ),
+
+
+  // Layer 4 - Reverse of Colemak Mod DH software mapping to QWERTY
+  [L_4] = LAYOUT_kc(
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
          ,    ,    , K  , S  , F  ,                O  , I  , L  ,SCLN, R  ,    ,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
