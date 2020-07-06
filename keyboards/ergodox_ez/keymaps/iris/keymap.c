06c2e0275507bbc327a3a8c47c545c866a3b660e
irh <ian.r.hobson@gmail.com>
2020-07-06 14:02:37 +0200

    Tweak Ergodox iris layout

--- keyboards/ergodox_ez/keymaps/iris/keymap.c
+++ keyboards/ergodox_ez/keymaps/iris/keymap.c
@@ -42,21 +42,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F3,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   _______,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   _______,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
-        _______,  TG(L_3), KC_LCTL, KC_LALT, KC_LCMD, LT(L_1, KC_BSPC), LT(L_2, KC_TAB),
-        _______, _______, _______, _______, _______,
+        LCAG(KC_H), TG(L_3), KC_LCTL, KC_LALT, KC_LCMD, LT(L_1, KC_BSPC), LT(L_2, KC_TAB),
+        _______, _______, KC_F15,  KC_F16,  LT(L_3, KC_F17),
                                                      _______, _______,
                                                               _______,
-                                            KC_F15,  KC_F16,  LCAG(KC_LEFT),
+                                            _______, _______, _______,
 
         // right hand
         _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MPLY,
         _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
                  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        LT(L_2, KC_ENTER), LT(L_1, KC_SPACE), KC_RCMD, KC_RALT, KC_RCTL, _______, _______,
-                 _______, _______, _______, _______, _______,
+        LT(L_2, KC_ENTER), LT(L_1, KC_SPACE), KC_RCMD, KC_RALT, KC_RCTL, LCAG(KC_LEFT), LCAG(KC_RIGHT),
+                 KC_F18,  KC_F19,  KC_F20,  _______, _______,
         _______, _______,
         _______,
-        LCAG(KC_RIGHT),  KC_F17,  KC_F18
+        _______, _______, _______
     ),
 
 /* Layer 1 - symbols
